6060ed3abe2d4a7a6c8baf3ed241b1ae02312982
Noumsmam <noumsrasolondraibe@gmail.com>
2025-06-24 20:00:08 +0300

    changement

--- test_db-master/assets/inc/fonction.php
+++ test_db-master/assets/inc/fonction.php
@@ -2,7 +2,12 @@
     require('connect.php');
     function getDepartement()
     {
-        $req="SELECT * FROM departments ;";
+        $req="SELECT departments.dept_name,employees.first_name,employees.last_name
+            FROM departments 
+            JOIN dept_manager 
+            JOIN employees
+            ON departments.dept_no = dept_manager.dept_no
+            AND dept_manager.emp_no = employees.emp_no ;";
         $sql=mysqli_query(dbconnect(),$req);
         $result=[];
         while( $row=mysqli_fetch_array($sql) )
--- test_db-master/page/liste_departement.php
+++ test_db-master/page/liste_departement.php
@@ -1,6 +1,6 @@
 <?php
     include('../assets/inc/fonction.php');
-    $_liste = getDepartement();
+    $liste = getDepartement();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,14 +8,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/bootstrap/js/bootstrap.bundle.min.js">
 </head>
 <body>
     <main>
         <div class="container-fluid">
             <table>
                 <tr>
-                    <td></td>
+                    <th>Departments</th>
+                    <th>Manager</th>
+                    <th>Bouton Action</th>
                 </tr>
+                <?php foreach($liste as $row) { ?>
+                    <tr>
+                        <td><?php echo $row[''] ?></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                <?php } ?>
             </table>
         </div>
     </main>
