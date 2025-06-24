9ec805256918733da13922d509263d63d14ea83b
Noumsmam <noumsrasolondraibe@gmail.com>
2025-06-24 21:26:16 +0300

    last

--- test_db-master/page/employees.php
+++ test_db-master/page/employees.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employees</title>
+    <link rel="stylesheet" href="../assets/bootstrap/js/bootstrap.bundle.min.js">
 </head>
 <body>
     <main>
