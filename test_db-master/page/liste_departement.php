516fc92c3ff4be6a46efd6b0e15f1f45ad349ead
Noumsmam <noumsrasolondraibe@gmail.com>
2025-06-25 11:02:32 +0300

    mety

--- test_db-master/page/liste_departement.php
+++ test_db-master/page/liste_departement.php
@@ -12,6 +12,9 @@
     <link rel="stylesheet" href="../assets/bootstrap/js/bootstrap.bundle.min.js">
 </head>
 <body>
+    <header>
+        <a href="rechercher.php"><button>Recherche</button></a>
+    </header>
     <main>
         <div class="col-8 text-center mx-auto ">
             <table class="table table-striped border">
