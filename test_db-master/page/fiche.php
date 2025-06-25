0cd3e51fdf93fa5a382cd1399a258a9064eca77c
Noumsmam <noumsrasolondraibe@gmail.com>
2025-06-25 09:44:23 +0300

    mety

--- test_db-master/page/fiche.php
+++ test_db-master/page/fiche.php
@@ -1,5 +1,20 @@
 <?php
-
+    include('../assets/inc/fonction.php');
+    $id=$_GET['id'];
+    $fiche=getFicheEmployee($id);
+    $first;
+    $last;
+    $salaire;
+    $from;
+    $to;
+    foreach($fiche as $row)
+    {
+        $first=$row['first_name'];
+        $last=$row['last_name'];
+        $salaire=$row['salary'];
+        $to=$row['to_date'];
+        $from=$row['from_date'];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +26,19 @@
 <body>
     <main>
         <div class="col-8">
-            
+            <p><?php echo $first; ?> <?php echo $last ?></p>
+            <table>
+                <tr>
+                    <th>Salary</th>
+                    <th>From</th>
+                    <th>To</th>
+                </tr>
+                <tr>
+                    <td><?php echo $salaire ?></td>
+                    <td><?php echo $from ?></td>
+                    <td><?php echo $to ?></td>
+                </tr>
+            </table>
         </div>
     </main>
 </body>
