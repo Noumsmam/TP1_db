f343a22b854ee53f7828e4ea7d16edbfdedbb055
Noumsmam <noumsrasolondraibe@gmail.com>
2025-06-25 10:01:37 +0300

    fiche

--- test_db-master/page/fiche.php
+++ test_db-master/page/fiche.php
@@ -11,9 +11,9 @@
     {
         $first=$row['first_name'];
         $last=$row['last_name'];
-        $salaire=$row['salary'];
-        $to=$row['to_date'];
-        $from=$row['from_date'];
+        // $salaire=$row['salary'];
+        // $to=$row['to_date'];
+        // $from=$row['from_date'];
     }
 ?>
 <!DOCTYPE html>
@@ -22,24 +22,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../assets/bootstrap/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
     <main>
         <div class="col-8">
-            <p><?php echo $first; ?> <?php echo $last ?></p>
-            <table>
+            <h2><?php echo $first; ?> <?php echo $last ?></h2>
+            <table class="table table-spired">
                 <tr>
                     <th>Salary</th>
                     <th>From</th>
                     <th>To</th>
                 </tr>
-                <tr>
-                    <td><?php echo $salaire ?></td>
-                    <td><?php echo $from ?></td>
-                    <td><?php echo $to ?></td>
-                </tr>
+                <?php foreach($fiche as $row) { ?>
+                    <tr>
+                        <td><?php echo $row['salary'] ?></td>
+                        <td><?php echo $row['from_date']  ?></td>
+                        <td><?php echo  $row['to_date'] ?></td>
+                    </tr>
+                <?php } ?>
             </table>
         </div>
+        <a href="liste_departement.php"><button class="btn btn-primary">retour</button></a>
     </main>
 </body>
 </html>
