7c4f154345f0c3d9f164617550c424b74a1ddd16
Noumsmam <noumsrasolondraibe@gmail.com>
2025-07-13 10:36:36 +0300

    nouveau fonction

--- test_db-master/assets/inc/fonction.php
+++ test_db-master/assets/inc/fonction.php
@@ -3,7 +3,7 @@
     function getDepartement()
     {
         $now=date("Y-m-d");
-        $req="SELECT departments.dept_name,employees.first_name,employees.last_name,departments.dept_no
+        $req="CREATE OR REPLACE VIEW v_departement AS SELECT departments.dept_name,employees.first_name,employees.last_name,departments.dept_no
             FROM departments 
             JOIN dept_manager 
             JOIN employees
@@ -22,7 +22,7 @@
 
     function getDepartementEmployees($id)
     {
-        $req="SELECT employees.emp_no,employees.first_name,employees.last_name,employees.birth_date,employees.gender,employees.hire_date
+        $req="CREATE OR REPLACE VIEW v_emp_dept AS SELECT employees.emp_no,employees.first_name,employees.last_name,employees.birth_date,employees.gender,employees.hire_date
         FROM employees 
         JOIN departments
         JOIN dept_emp
@@ -40,7 +40,7 @@
 
     function getFicheEmployee($id)
     {
-        $req="SELECT employees.first_name,employees.last_name,employees.gender,
+        $req="CREATE OR REPLACE VIEW v_fiche_emp AS SELECT employees.first_name,employees.last_name,employees.gender,
         salaries.salary,salaries.from_date,salaries.to_date,departments.dept_name
         FROM employees
         JOIN salaries
@@ -60,16 +60,17 @@
         return $result;
     }
 
-    function rechercher($bdd, $numDept, $nom, $min, $max, $page){
+    function rechercher($numDept, $nom, $min, $max, $page){
         $limit = $page * 20;
-        $sql = "SELECT employees.*, TIMESTAMPDIFF(YEAR, employees.birth_date, CURDATE()) as Age FROM employees
+        $sql = "CREATE OR REPLACE VIEW v_recherche AS SELECT employees.*, TIMESTAMPDIFF(YEAR, employees.birth_date, CURDATE()) as Age FROM employees
                 JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-                WHERE dept_emp.dept_no = '%s' 
-                AND first_name LIKE '%%%s%%' 
+                WHERE 1=1 
+                AND dept_no='%s'
+                AND first_name LIKE '%s' 
                 AND TIMESTAMPDIFF(YEAR, birth_date, CURDATE()) > '%s' 
                 AND TIMESTAMPDIFF(YEAR, birth_date, CURDATE()) < '%s' LIMIT %s, 20;";
         $sql = sprintf($sql, $numDept, $nom, $min, $max, $limit);
-        $requete = mysqli_query($bdd, $sql);
+        $requete = mysqli_query(dbconnect(), $sql);
         return $requete;
     }
 ?>
